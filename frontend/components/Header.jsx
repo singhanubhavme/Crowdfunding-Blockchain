293bde04b643c1252ad88e70e1b310f4fcda2702
Anubhav Singh <singhanubhav58@gmail.com>
2023-01-10 02:36:54 +0530

    added connect button

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,9 @@
+import { ConnectButton } from "web3uikit"
+export default function Header() {
+    return (
+        <div>
+            Decentralized Fundraiser
+            <ConnectButton />
+        </div>
+    );
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Decentralized Fundraiser</title>
+        <meta name="description" content="Decentralized Fundraiser for everyone" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
