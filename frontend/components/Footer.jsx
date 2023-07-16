d0ed4ef73124a1d2ded51e8103dfc0a54f2237cb
Anubhav Singh <singhanubhav58@gmail.com>
2023-07-16 09:33:52 +0530

    updated footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,36 +1,36 @@
 const Footer = () => {
   return (
-    <footer class="text-gray-400 bg-gray-900 body-font">
-      <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <footer className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a
           href="/"
           className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
         >
           <span className="ml-3 text-xl">Decentralized Fundraiser</span>
         </a>
-        <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
+        <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
           © 2023 Anubhav —
           <a
             href="https://instagram.com/anubhavsinghdev"
-            class="text-gray-500 ml-1"
+            className="text-gray-500 ml-1"
             target="_blank"
             rel="noopener noreferrer"
           >
             @anubhavsinghdev
           </a>
         </p>
-        <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start mr-4">
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start mr-4">
           <a
             href="https://twitter.com/AnubhavSinghDev"
             target="_blank"
-            class="ml-3 text-gray-400"
+            className="ml-3 text-gray-400"
           >
             <svg
               fill="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               stroke-width="2"
-              class="w-5 h-5"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
@@ -39,7 +39,7 @@ const Footer = () => {
           <a
             href="https://www.instagram.com/anubhavsinghdev/"
             target="_blank"
-            class="ml-3 text-gray-400"
+            className="ml-3 text-gray-400"
           >
             <svg
               fill="none"
@@ -47,7 +47,7 @@ const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               stroke-width="2"
-              class="w-5 h-5"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -57,15 +57,15 @@ const Footer = () => {
           <a
             href="https://www.linkedin.com/in/anubhavsinghdev/"
             target="_blank"
-            class="ml-3 text-gray-400"
+            className="ml-3 text-gray-400"
           >
             <svg
               fill="currentColor"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
-              stroke-width="0"
-              class="w-5 h-5"
+              strokeWidth="0"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <path
